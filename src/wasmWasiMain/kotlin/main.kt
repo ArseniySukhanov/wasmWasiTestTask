49220d26c0568edacbef5fa44dd96a7c6f175f2c
Arsenii Sukhanov <31130789+ArseniySukhanov@users.noreply.github.com>
2026-05-08 17:02:05 +0200

    feat: add readln2
    add readln2 which makes what readln suppose to do

--- src/wasmWasiMain/kotlin/main.kt
+++ src/wasmWasiMain/kotlin/main.kt
@@ -1,17 +1,52 @@
+@file:OptIn(UnsafeWasmMemoryApi::class)
+
+import kotlin.wasm.unsafe.MemoryAllocator
+import kotlin.wasm.unsafe.Pointer
+import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
+import kotlin.wasm.unsafe.withScopedMemoryAllocator
+
 private const val STDIN=0
 
 @ExperimentalWasmInterop
 @WasmImport("wasi_snapshot_preview1", "fd_read")
 external fun wasiRawFdRead(descriptor: Int, scatterPtr: Int, scatterSize: Int, errorPtr: Int): Int
 
+@OptIn(ExperimentalWasmInterop::class)
+internal fun wasiReadImpl(
+    allocator: MemoryAllocator,
+    nullable: Boolean
+):List<Byte>?{
+    val res:MutableList<Byte> = mutableListOf()
+    val ptr=allocator.allocate(1)
+    val scatterPtr=allocator.allocate(8)
+    (scatterPtr+0).storeInt(ptr.address.toInt())
+    (scatterPtr+4).storeInt(1)
+
+    val rp0 = allocator.allocate(4)
+    do{
+        val ret = wasiRawFdRead(
+            descriptor = STDIN,
+            scatterPtr = scatterPtr.address.toInt(),
+            scatterSize = 1,
+            errorPtr = rp0.address.toInt()
+        )
+        res.add(ptr.loadByte())
+    }while(ptr.loadByte().toInt() !=0x0A)
+    res.removeLast()
+    return res
+}
+
 fun readln2():String{
-    return ""
+    return withScopedMemoryAllocator { allocator ->
+        wasiReadImpl(allocator=allocator, nullable=false)
+    }?.toString() as String
 }
 
 fun main(){
     var s: String
     while (true){
+        println("STARTING")
         s=readln2()
-        println(s)
+        println("Wasm received: $s")
     }
 }
