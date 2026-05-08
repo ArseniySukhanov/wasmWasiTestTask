4591fae3a136706c01ece5c4d887f2c7ef0c6243
Arsenii Sukhanov <31130789+ArseniySukhanov@users.noreply.github.com>
2026-05-08 17:46:46 +0200

    feat: add an exception handling
    add an exception handling and readlnOrNull()

--- src/wasmWasiMain/kotlin/stdi.kt
+++ src/wasmWasiMain/kotlin/stdi.kt
@@ -1,5 +1,6 @@
 @file:OptIn(UnsafeWasmMemoryApi::class)
 
+
 import kotlin.wasm.unsafe.MemoryAllocator
 import kotlin.wasm.unsafe.UnsafeWasmMemoryApi
 import kotlin.wasm.unsafe.withScopedMemoryAllocator
@@ -15,7 +16,7 @@ internal fun wasiReadImpl(
     allocator: MemoryAllocator,
     nullable: Boolean
 ):ByteArray?{
-    val res:MutableList<Byte> = mutableListOf()
+    val tmpByteList:MutableList<Byte> = mutableListOf()
     val ptr=allocator.allocate(1)
     val scatterPtr=allocator.allocate(8)
     (scatterPtr+0).storeInt(ptr.address.toInt())
@@ -29,14 +30,27 @@ internal fun wasiReadImpl(
             scatterSize = 1,
             errorPtr = rp0.address.toInt()
         )
-        res.add(ptr.loadByte())
-    }while(ptr.loadByte().toInt() !=0x0A)
-    res.removeLast()
-    return ByteArray(res.size){i -> res[i]}
+        if (ret!=0) {
+            if (nullable){
+                return null
+            }
+            else{
+                throw WasiError(WasiErrorCode.entries[ret])
+            }
+        }
+        tmpByteList.add(ptr.loadByte())
+    }while(ptr.loadByte().toInt() != 0x0A)
+    return ByteArray(tmpByteList.size-1){i -> tmpByteList[i]}
 }
 
 fun readln():String{
     return withScopedMemoryAllocator { allocator ->
         wasiReadImpl(allocator=allocator, nullable=false)
     }?.decodeToString() as String
+}
+
+fun readlnOrNull():String?{
+    return withScopedMemoryAllocator { allocator ->
+        wasiReadImpl(allocator=allocator, nullable=true)
+    }?.decodeToString()
 }
