a5fcd45837bfcc63b3353f71aaf92831231cb4bb
Arsenii Sukhanov <31130789+ArseniySukhanov@users.noreply.github.com>
2026-05-08 13:22:46 +0200

    feat: add dummy implementation of readln() and import wasi fd_read

--- src/wasmWasiMain/kotlin/main.kt
+++ src/wasmWasiMain/kotlin/main.kt
@@ -1,7 +1,17 @@
+private const val STDIN=0
+
+@ExperimentalWasmInterop
+@WasmImport("wasi_snapshot_preview1", "fd_read")
+external fun wasiRawFdRead(descriptor: Int, scatterPtr: Int, scatterSize: Int, errorPtr: Int): Int
+
+fun readln2():String{
+    return ""
+}
+
 fun main(){
     var s: String
     while (true){
-        s=readln()
+        s=readln2()
         println(s)
     }
 }
